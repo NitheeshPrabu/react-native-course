ab00087e283ad131b66a167fd93f01e0019d9735
Shanmugasundaram Nitheesh Prabu <Nitheesh.Prabu.Shanmugasundaram@navis.com>
2019-08-03 12:12:18 +0530

    logout

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: null
+    };
+  }
+
   componentDidMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyC9U5gykpt0SaAxYbsnmkjJUSOXqVnQgog',
@@ -16,13 +23,36 @@ class App extends Component {
       messagingSenderId: '140365694200',
       appId: '1:140365694200:web:4790f4e9a6fdd4d3'
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
