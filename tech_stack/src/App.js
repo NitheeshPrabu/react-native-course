065a4aa7e73b1eef9263d7789f8ba8f0a6c646ec
Shanmugasundaram Nitheesh Prabu <Nitheesh.Prabu.Shanmugasundaram@navis.com>
2019-08-03 21:02:38 +0530

    Initial Commit

---
+++ tech_stack/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers';
+import { Header } from './components/common';
+
+const App = () => (
+  <Provider store={createStore(reducers)}>
+    <View>
+      <Header headerText="Tech Stack" />
+    </View>
+  </Provider>
+);
+
+export default App;
