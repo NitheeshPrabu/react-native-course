9f32065be83c65904cc310d4989ceda3dddc43ca
Shanmugasundaram Nitheesh Prabu <Nitheesh.Prabu.Shanmugasundaram@navis.com>
2019-08-03 23:31:03 +0530

    redux + anims

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, NativeModules } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import reducers from './reducers';
 import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
-const App = () => (
-  <Provider store={createStore(reducers)}>
-    <View>
-      <Header headerText="Tech Stack" />
-    </View>
-  </Provider>
-);
+const { UIManager } = NativeModules;
 
-export default App;
+export default class App extends Component {
+  constructor() {
+    super();
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1 }}>
+          <Header headerText="Tech Stack" />
+          <LibraryList />
+        </View>
+      </Provider>
+    );
+  }
+}
---
+++ tech_stack/src/components/LibraryList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
+
+import ListItem from './ListItem';
+
+class LibraryList extends Component {
+  renderItem(library) {
+    return <ListItem library={library.item} />;
+  }
+
+  render() {
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={(library, index) => `librarylist-item-${index}`}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  libraries: state.libraries
+});
+
+export default connect(mapStateToProps)(LibraryList);
