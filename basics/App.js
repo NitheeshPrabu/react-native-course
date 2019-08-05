5242f236f204e0fd73fc45982a393c8a266ddaf7
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2019-08-05 23:23:55 +0530

    Initial Commit

---
+++ basics/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+import placeImage from './src/assets/beautiful-place.jpg';
+
+export default class App extends Component {
+  state = {
+    places: []
+  };
+
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat({
+          key: Math.random().toString(),
+          name: placeName,
+          image: placeImage
+        })
+      };
+    });
+  };
+
+  placeDeletedHandler = key => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter(place => {
+          return place.key !== key;
+        })
+      };
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
+          places={this.state.places}
+          onItemDeleted={this.placeDeletedHandler}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
+});
