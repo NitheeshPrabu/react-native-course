95d1c6010c60c353a0876919268318bc27a8b05b
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2019-08-07 22:54:31 +0530

    login using otp

--- firebase-auth-otp/App.js
+++ firebase-auth-otp/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import firebase from 'firebase';
 
 import SignUpForm from './src/components/SignUpForm';
+import SignInForm from './src/components/SignInForm';
 
 class App extends Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyAPmo9ZPG0DmwsShFWc7XN3xd8N2LsKl0g",
+      authDomain: "react-native-otp-e1089.firebaseapp.com",
+      databaseURL: "https://react-native-otp-e1089.firebaseio.com",
+      projectId: "react-native-otp-e1089",
+      storageBucket: "",
+      messagingSenderId: "946903621999",
+      appId: "1:946903621999:web:8c9cc6ef5df92120"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(config);
+  }
+  
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -18,7 +35,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
 
--- firebase-auth-otp/src/components/SignInForm.js
+++ firebase-auth-otp/src/components/SignInForm.js
@@ -2,30 +2,21 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Input, Button } from 'react-native-elements';
 import axios from 'axios';
+import firebase from 'firebase';
 
 const ROOT_URL = 'https://us-central1-react-native-otp-e1089.cloudfunctions.net';
 
-class SignUpForm extends Component {
+class SignInForm extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { phone: '' };
+		this.state = { phone: '', code: '' };
 	}
 
-	// handleSubmit = () => {
-	// 	axios.post(`${ROOT_URL}/createUser`, {
-	// 		phone: this.state.phone	
-	// 	}).then(user => {
-	// 		axios.post(`${ROOT_URL}/requestOtp`, {
-	// 			phone: this.state.phone
-	// 		});
-	// 	})
-	// }
-
-	// arrow function, dont have to use .bind(this)
 	handleSubmit = async () => {
+		const { phone, code } = this.state;
 		try {
-			await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });
-			await axios.post(`${ROOT_URL}/requestOtp`, { phone: this.state.phone });
+			let { data } = await axios.post(`${ROOT_URL}/verifyOtp`, { phone, code });
+			firebase.auth().signInWithCustomToken(data.token);
 		} catch (err) {
 			console.log(err);
 		}
@@ -41,10 +32,17 @@ class SignUpForm extends Component {
 						label="Enter Phone Number"
 					/>
 				</View>
+				<View style={{ marginBottom: 10 }}>
+					<Input
+						value={this.state.code}
+						onChangeText={code => this.setState({ code })}
+						label="Enter OTP"
+					/>
+				</View>
 				<Button onPress={this.handleSubmit} title="Submit" />
 			</View>
 		);
 	}
 }
 
-export default SignUpForm;
+export default SignInForm;
