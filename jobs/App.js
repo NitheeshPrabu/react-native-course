15f62dffb85e8ccfa9fee46482999607b9f93dab
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2019-08-09 21:00:04 +0530

    push notifications

--- jobs/App.js
+++ jobs/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Alert } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Notifications } from 'expo';
 
 import Navigation from './src/RootNavigator';
 import configureStore from './src/store';
+import registerForPushNotifications from './src/services/pushNotifications';
 
 const { store, persistor } = configureStore();
 
 export default class App extends Component {
+  componentDidMount() {
+    registerForPushNotifications();
+    Notifications.addListener(notification => {
+      const {
+        data: { text },
+        origin
+      } = notification;
+      if (origin === 'received' && text) {
+        Alert.alert('New Push Notification', text, [{ text: 'Ok.' }]);
+      }
+    });
+  }
+
   render() {
     return (
       <Provider store={store}>
---
+++ jobs/src/services/pushNotifications.js
@@ -0,0 +1,30 @@
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import { AsyncStorage } from 'react-native';
+import axios from 'axios';
+
+const PUSH_ENDPOINT = 'http://rallycoding.herokuapp.com/api/tokens';
+
+export default async () => {
+  const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+  let finalStatus = existingStatus;
+
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== 'granted') {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== 'granted') {
+    return;
+  }
+
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+  await axios.post(PUSH_ENDPOINT, { token: { token } });
+  AsyncStorage.setItem('PUSH_TOKEN', token);
+};
