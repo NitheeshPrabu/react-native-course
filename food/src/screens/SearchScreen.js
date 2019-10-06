179c472b01710a74d5316e51be22ea397ce85737
NitheeshPrabu <nitheeshprabu.s@gmail.com>
2019-10-06 15:33:15 +0530

    searchscreen and searchbar

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import SearchBar from '../components/SearchBar';
+
 const SearchScreen = props => {
+  const [term, setTerm] = useState('');
   return (
     <View>
-      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={newTerm => setTerm(newTerm)}
+        onTermSubmit={() => console.log('term was submitted')}
+      />
     </View>
   );
 };
